1d1adc6cfdc166928ca954356dec063cc6e596f1
victoryang00 <yangyiwei2000@gmail.com>
2021-10-13 05:52:41 -0400

    compilable

--- main.cu
+++ main.cu
@@ -1,14 +1,16 @@
+#define VKFFT_BACKEND 1
+
 #include <iostream>
-#include <utils_VkFFT.h>
 #include <cufftw.h>
 #include <chrono>
+#include <vkFFT.h>
 
 struct interfaceFFTPlan {
     VkFFTConfiguration *config;
     VkFFTApplication *app;
+    VkFFTLaunchParams *lParams;
     bool isBaked;
     bool notInit;
-    VkFFTLaunchParams *lParams;
     CUdevice device;
     CUcontext context;
     int dataType;
@@ -63,187 +65,10 @@ performVulkanFFTiFFT(interfaceFFTPlan *plan, uint64_t num_iter) {
     return resFFT;
 }
 
-VkFFTResult get_VkFFT_double(VkGPU *vkGPU) {
-    VkFFTResult resFFT = VKFFT_SUCCESS;
-
-    cudaError_t res = cudaSuccess;
-
-//    fftw_complex *inputC;
-//    fftw_complex *inputC_double;
-//    uint64_t dims[3] = {benchmark_dimensions[n][0], benchmark_dimensions[n][1], benchmark_dimensions[n][2]};
-//
-//    inputC = (fftw_complex *) (malloc(sizeof(fftw_complex) * dims[0] * dims[1] * dims[2]));
-//    if (!inputC) return VKFFT_ERROR_MALLOC_FAILED;
-//    inputC_double = (fftw_complex *) (malloc(sizeof(fftw_complex) * dims[0] * dims[1] * dims[2]));
-//    if (!inputC_double) return VKFFT_ERROR_MALLOC_FAILED;
-//    for (uint64_t l = 0; l < dims[2]; l++) {
-//        for (uint64_t j = 0; j < dims[1]; j++) {
-//            for (uint64_t i = 0; i < dims[0]; i++) {
-//                inputC[i + j * dims[0] + l * dims[0] * dims[1]][0] = (double) (
-//                        2 * ((double) rand()) / RAND_MAX - 1.0);
-//                inputC[i + j * dims[0] + l * dims[0] * dims[1]][1] = (double) (
-//                        2 * ((double) rand()) / RAND_MAX - 1.0);
-//                inputC_double[i + j * dims[0] + l * dims[0] * dims[1]][0] = (double) inputC[i + j * dims[0] +
-//                                                                                            l * dims[0] *
-//                                                                                            dims[1]][0];
-//                inputC_double[i + j * dims[0] + l * dims[0] * dims[1]][1] = (double) inputC[i + j * dims[0] +
-//                                                                                            l * dims[0] *
-//                                                                                            dims[1]][1];
-//            }
-//        }
-//    }
-//
-//    fftw_plan p;
-//
-//    fftw_complex *output_FFTW = (fftw_complex *) (malloc(sizeof(fftw_complex) * dims[0] * dims[1] * dims[2]));
-//    if (!output_FFTW) return VKFFT_ERROR_MALLOC_FAILED;
-//    switch (benchmark_dimensions[n][3]) {
-//        case 1:
-//            p = fftw_plan_dft_1d((int) benchmark_dimensions[n][0], inputC_double, output_FFTW, -1,
-//                                 FFTW_ESTIMATE);
-//            break;
-//        case 2:
-//            p = fftw_plan_dft_2d((int) benchmark_dimensions[n][1], (int) benchmark_dimensions[n][0],
-//                                 inputC_double, output_FFTW, -1, FFTW_ESTIMATE);
-//            break;
-//        case 3:
-//            p = fftw_plan_dft_3d((int) benchmark_dimensions[n][2], (int) benchmark_dimensions[n][1],
-//                                 (int) benchmark_dimensions[n][0], inputC_double, output_FFTW, -1,
-//                                 FFTW_ESTIMATE);
-//            break;
-//    }
-//
-//    fftw_execute(p);
-
-    float totTime = 0;
-    int num_iter = 1;
-
-    //VkFFT part
-
-    VkFFTConfiguration configuration = {};
-    VkFFTApplication app = {};
-
-    configuration.FFTdim = benchmark_dimensions[n][3]; //FFT dimension, 1D, 2D or 3D (default 1).
-    configuration.size[0] = benchmark_dimensions[n][0]; //Multidimensional FFT dimensions sizes (default 1). For best performance (and stability), order dimensions in descendant size order as: x>y>z.
-    configuration.size[1] = benchmark_dimensions[n][1];
-    configuration.size[2] = benchmark_dimensions[n][2];
-
-    //After this, configuration file contains pointers to Vulkan objects needed to work with the GPU: VkDevice* device - created device, [uint64_t *bufferSize, VkBuffer *buffer, VkDeviceMemory* bufferDeviceMemory] - allocated GPU memory FFT is performed on. [uint64_t *kernelSize, VkBuffer *kernel, VkDeviceMemory* kernelDeviceMemory] - allocated GPU memory, where kernel for convolution is stored.
-    configuration.device = &vkGPU->device;
-    configuration.doublePrecision = true;
-
-    uint64_t numBuf = 1;
-
-    //Allocate buffers for the input data. - we use 4 in this example
-    uint64_t *bufferSize = (uint64_t *) malloc(sizeof(uint64_t) * numBuf);
-    if (!bufferSize) return VKFFT_ERROR_MALLOC_FAILED;
-    for (uint64_t i = 0; i < numBuf; i++) {
-        bufferSize[i] = {};
-        bufferSize[i] = (uint64_t) sizeof(double) * 2 * configuration.size[0] * configuration.size[1] *
-                        configuration.size[2] / numBuf;
-    }
-
-    cuFloatComplex *buffer = 0;
-
-    for (uint64_t i = 0; i < numBuf; i++) {
-
-        res = cudaMalloc((void **) &buffer, bufferSize[i]);
-        if (res != cudaSuccess) return VKFFT_ERROR_FAILED_TO_ALLOCATE;
-
-    }
-
-    configuration.bufferNum = numBuf;
-    // Can specify buffers at launch
-    configuration.bufferSize = bufferSize;
-
-    //Sample buffer transfer tool. Uses staging buffer of the same size as destination buffer, which can be reduced if transfer is done sequentially in small buffers.
-    uint64_t shift = 0;
-    for (uint64_t i = 0; i < numBuf; i++) {
-
-        res = cudaMemcpy(buffer, inputC, bufferSize[i], cudaMemcpyHostToDevice);
-        if (res != cudaSuccess) return VKFFT_ERROR_FAILED_TO_COPY;
-
-        shift += bufferSize[i];
-    }
-    //Initialize applications. This function loads shaders, creates pipeline and configures FFT based on configuration file. No buffer allocations inside VkFFT library.
-    resFFT = initializeVkFFT(&app, configuration);
-    if (resFFT != VKFFT_SUCCESS) return resFFT;
-    //Submit FFT+iFFT.
-    //num_iter = 1;
-    //specify buffers at launch
-    VkFFTLaunchParams launchParams = {};
-
-    launchParams.buffer = (void **) &buffer;
-
-    resFFT = performVulkanFFT(vkGPU, &app, &launchParams, -1, num_iter);
-    if (resFFT != VKFFT_SUCCESS) return resFFT;
-    fftw_complex *output_VkFFT = (fftw_complex *) (malloc(sizeof(fftw_complex) * dims[0] * dims[1] * dims[2]));
-    if (!output_VkFFT) return VKFFT_ERROR_MALLOC_FAILED;
-    //Transfer data from GPU using staging buffer.
-    shift = 0;
-    for (uint64_t i = 0; i < numBuf; i++) {
-
-        res = cudaMemcpy(output_VkFFT, buffer, bufferSize[i], cudaMemcpyDeviceToHost);
-        if (res != cudaSuccess) return VKFFT_ERROR_FAILED_TO_COPY;
-
-        shift += bufferSize[i];
-    }
-    double avg_difference[2] = {0, 0};
-    double max_difference[2] = {0, 0};
-    double avg_eps[2] = {0, 0};
-    double max_eps[2] = {0, 0};
-    for (uint64_t l = 0; l < dims[2]; l++) {
-        for (uint64_t j = 0; j < dims[1]; j++) {
-            for (uint64_t i = 0; i < dims[0]; i++) {
-                uint64_t loc_i = i;
-                uint64_t loc_j = j;
-                uint64_t loc_l = l;
-
-                double current_data_norm = sqrt(output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][0] *
-                                                output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][0] +
-                                                output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][1] *
-                                                output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][1]);
-
-                double current_diff_x_VkFFT = (
-                        output_VkFFT[loc_i + loc_j * dims[0] + loc_l * dims[0] * dims[1]][0] -
-                        output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][0]);
-                double current_diff_y_VkFFT = (
-                        output_VkFFT[loc_i + loc_j * dims[0] + loc_l * dims[0] * dims[1]][1] -
-                        output_FFTW[i + j * dims[0] + l * dims[0] * dims[1]][1]);
-                double current_diff_norm_VkFFT = sqrt(current_diff_x_VkFFT * current_diff_x_VkFFT +
-                                                      current_diff_y_VkFFT * current_diff_y_VkFFT);
-                if (current_diff_norm_VkFFT > max_difference[1]) max_difference[1] = current_diff_norm_VkFFT;
-                avg_difference[1] += current_diff_norm_VkFFT;
-                if ((current_diff_norm_VkFFT / current_data_norm > max_eps[1]) && (current_data_norm > 1e-16)) {
-                    max_eps[1] = current_diff_norm_VkFFT / current_data_norm;
-                }
-                avg_eps[1] += (current_data_norm > 1e-10) ? current_diff_norm_VkFFT / current_data_norm : 0;
-            }
-        }
-    }
-    avg_difference[0] /= (dims[0] * dims[1] * dims[2]);
-    avg_eps[0] /= (dims[0] * dims[1] * dims[2]);
-    avg_difference[1] /= (dims[0] * dims[1] * dims[2]);
-    avg_eps[1] /= (dims[0] * dims[1] * dims[2]);
-    printf("VkFFT System: %" PRIu64 "x%" PRIu64 "x%" PRIu64 " avg_difference: %.15f max_difference: %.15f avg_eps: %.15f max_eps: %.15f\n",
-           dims[0], dims[1], dims[2], avg_difference[1], max_difference[1], avg_eps[1], max_eps[1]);
-    free(output_VkFFT);
-    for (uint64_t i = 0; i < numBuf; i++) {
-        cudaFree(buffer);
-    }
-    deleteVkFFT(&app);
-    free(inputC);
-    fftw_destroy_plan(p);
-    free(inputC_double);
-    free(output_FFTW);
-
-
-    return resFFT;
-}
 
 //vkfftPlanMany
 VkFFTResult
-vkfftPlanMany(VkGPU *vkGPU, VkFFTConfiguration configuration, VkFFTApplication appZ2Z, int rank, int *doubleComplex,
+vkfftPlanMany(interfaceFFTPlan *interface, int rank, int *doubleComplex,
               int *doubleComplexPadded, int istride,
               int doubleComplexPaddedTotal, int *complexGridSizePadded, int ostride,
               int complexGridSizePaddedTotal, cufftType type, int batch, cudaStream_t *stream) {
@@ -251,47 +76,49 @@ vkfftPlanMany(VkGPU *vkGPU, VkFFTConfiguration configuration, VkFFTApplication a
     cuDoubleComplex *buffer1 = 0;
 
     const int ZZ = 3, XX = 1, YY = 2;
-    configuration.FFTdim = 3;
-    configuration.size[0] = doubleComplex[ZZ];
-    configuration.size[1] = doubleComplex[XX];
-    configuration.size[2] = doubleComplex[YY];
-    configuration.doublePrecision = true;
+    interface->config->FFTdim = 3;
+    interface->config->size[0] = doubleComplex[ZZ];
+    interface->config->size[1] = doubleComplex[XX];
+    interface->config->size[2] = doubleComplex[YY];
+    interface->config->doublePrecision = true;
     //configuration.disableMergeSequencesR2C = 1;
-    configuration.device = (CUdevice *) malloc(sizeof(CUdevice));
-    cudaError_t result = cudaGetDevice(configuration.device);
+    interface->config->device = (CUdevice *) malloc(sizeof(CUdevice));
+    cudaError_t result = cudaGetDevice(interface->config->device);
     if (result != cudaSuccess) {
         printf("VKFFT_ERROR_FAILED_TO_GET_DEVICE error: %d\n", result);
         return VKFFT_ERROR_FAILED_TO_GET_DEVICE;
     }
-    configuration.num_streams = 1;
-    configuration.stream = stream;
+    interface->config->num_streams = 1;
+    interface->config->stream = stream;
 
     uint64_t bufferSize =
             complexGridSizePadded[XX] * complexGridSizePadded[YY] * complexGridSizePadded[ZZ] *
             sizeof(cufftDoubleComplex);
     std::cout << "bufferSize = " << bufferSize << std::endl;
-    configuration.bufferSize = &bufferSize;
-    configuration.bufferStride[0] = complexGridSizePadded[ZZ];
-    configuration.bufferStride[1] = complexGridSizePadded[ZZ] * complexGridSizePadded[YY];
-    configuration.bufferStride[2] = complexGridSizePadded[ZZ] * complexGridSizePadded[YY] * complexGridSizePadded[XX];
+    interface->config->bufferSize = &bufferSize;
+    interface->config->bufferStride[0] = complexGridSizePadded[ZZ];
+    interface->config->bufferStride[1] = complexGridSizePadded[ZZ] * complexGridSizePadded[YY];
+    interface->config->bufferStride[2] =
+            complexGridSizePadded[ZZ] * complexGridSizePadded[YY] * complexGridSizePadded[XX];
     result = cudaMalloc((void **) &buffer, bufferSize);
     if (result != cudaSuccess) return VKFFT_ERROR_FAILED_TO_ALLOCATE;
-    configuration.buffer = (void **) &buffer;
+    interface->config->buffer = (void **) &buffer;
 
 
-    configuration.isInputFormatted = 1;
-    configuration.inverseReturnToInputBuffer = 1;
+    interface->config->isInputFormatted = 1;
+    interface->config->inverseReturnToInputBuffer = 1;
     uint64_t inputBufferSize =
             doubleComplexPadded[XX] * doubleComplexPadded[YY] * doubleComplexPadded[ZZ] * sizeof(cufftDoubleComplex);
     std::cout << "inputBufferSize = " << inputBufferSize << std::endl;
-    configuration.inputBufferSize = &inputBufferSize;
-    configuration.inputBufferStride[0] = doubleComplexPadded[ZZ];
-    configuration.inputBufferStride[1] = doubleComplexPadded[ZZ] * doubleComplexPadded[YY];
-    configuration.inputBufferStride[2] = doubleComplexPadded[ZZ] * doubleComplexPadded[YY] * doubleComplexPadded[XX];
+    interface->config->inputBufferSize = &inputBufferSize;
+    interface->config->inputBufferStride[0] = doubleComplexPadded[ZZ];
+    interface->config->inputBufferStride[1] = doubleComplexPadded[ZZ] * doubleComplexPadded[YY];
+    interface->config->inputBufferStride[2] =
+            doubleComplexPadded[ZZ] * doubleComplexPadded[YY] * doubleComplexPadded[XX];
     result = cudaMemcpy(buffer, buffer1, inputBufferSize, cudaMemcpyHostToDevice);
     if (result != cudaSuccess) return VKFFT_ERROR_FAILED_TO_COPY;
-    configuration.inputBuffer = (void **) &buffer1;
-    VkFFTResult resFFT = initializeVkFFT(&appZ2Z, configuration);
+    interface->config->inputBuffer = (void **) &buffer1;
+    VkFFTResult resFFT = initializeVkFFT(interface->app, *interface->config);
     if (resFFT != VKFFT_SUCCESS) printf("VkFFT error: %d\n", resFFT);
 
 
@@ -300,30 +127,29 @@ vkfftPlanMany(VkGPU *vkGPU, VkFFTConfiguration configuration, VkFFTApplication a
     return resFFT;
 }
 
-VkFFTResult vkfftExecZ2Z(VkGPU *vkGPU, VkFFTApplication appZ2Z, VkFFTConfiguration configuration,
+VkFFTResult vkfftExecZ2Z(interfaceFFTPlan *inerface,
                          cufftDoubleComplex *idata,
                          cufftDoubleComplex *odata,
                          int direction, cudaStream_t *stream) {
-    uint64_t num_iter = (((uint64_t) 4096 * 1024.0 * 1024.0) / *configuration.bufferSize > 1000) ? 1000 :
-                        (uint64_t) ((uint64_t) 4096 * 1024.0 * 1024.0) / *configuration.bufferSize;
-    VkFFTLaunchParams launchParams = {};
-    auto resFFT = performVulkanFFTiFFT(vkGPU, &appZ2Z, &launchParams, num_iter);
+    uint64_t num_iter = (((uint64_t) 4096 * 1024.0 * 1024.0) / *(inerface->config->bufferSize) > 1000) ? 1000 :
+                        (uint64_t) ((uint64_t) 4096 * 1024.0 * 1024.0) / *inerface->config->bufferSize;
+    auto resFFT = performVulkanFFTiFFT(inerface, num_iter);
     return resFFT;
 }
 
-VkFFTResult vkfftBakeFFTPlan(interfaceFFTPlan* plan,cudaStream_t *stream) {
+VkFFTResult vkfftBakeFFTPlan(interfaceFFTPlan *plan, cudaStream_t *stream) {
     VkFFTResult res;
-#if(__DEBUG__>0)
+#if(__DEBUG__ > 0)
     printf("Begin initialization...\n");
 #endif
     // If the plan was baked previously, the previous plan needs to be deleted
     if ((plan->app != NULL) && (plan->isBaked)) {
         deleteVkFFT(plan->app);
-        plan->app = (VkFFTApplication*)calloc(1,sizeof(VkFFTApplication));
+        plan->app = (VkFFTApplication *) calloc(1, sizeof(VkFFTApplication));
     }
     VkFFTConfiguration tmpConfig = *plan->config;
     res = initializeVkFFT(plan->app, tmpConfig);
-#if(__DEBUG__>0)
+#if(__DEBUG__ > 0)
     printf("    Done with initialization...\n");
 #endif
 
@@ -350,9 +176,14 @@ VkFFTResult vkfftBakeFFTPlan(interfaceFFTPlan* plan,cudaStream_t *stream) {
 }
 
 int main() {
-    interfaceFFTPlan* plan = {};
-    cudaStream_t *t;
+    interfaceFFTPlan *plan_interface = {};
 
+    plan_interface = (interfaceFFTPlan *) malloc(sizeof(interfaceFFTPlan));
+    plan_interface->config = (VkFFTConfiguration *) malloc(sizeof(VkFFTConfiguration));
+    plan_interface->app = (VkFFTApplication *) malloc(sizeof(VkFFTApplication));
+    plan_interface->lParams = (VkFFTLaunchParams *) malloc(sizeof(VkFFTLaunchParams));
+
+    cudaStream_t *t;
 
 
     std::cout << "Second Test" << std::endl;
@@ -364,11 +195,6 @@ int main() {
             {(uint64_t) pow(2, 8), (uint64_t) pow(2, 8), (uint64_t) pow(2, 8), 3}
     };
 
-    for (int n = 0; n < num_benchmark_samples; n++) {
-        for (int r = 0; r < num_runs; r++) {
-            get_VkFFT_double()
-        }
-    }
     std::cout << "Third Test" << std::endl;
     double *signal = (double *) malloc(sizeof(double) * 256 * 10);
     int dim_arry[3] = {1, 1, 1};
@@ -397,11 +223,10 @@ int main() {
         data1[i].x = signal[i];
         data1[i].y = 0.;
     }
-    VkFFTConfiguration configuration = {};
-    VkFFTApplication appZ2Z = {};
-    vkfftPlanMany(&vkGPU, configuration, appZ2Z, 256, dim_arry, dim_arry, 1, 0,
+
+    vkfftPlanMany(plan_interface, 256, dim_arry, dim_arry, 1, 0,
                   dim_arry, 1, 0, CUFFT_Z2Z, 10, t);
-    vkfftExecZ2Z(&vkGPU, appZ2Z, configuration, data1, data1, CUFFT_FORWARD, t);
+    vkfftExecZ2Z(plan_interface, data1, data1, CUFFT_FORWARD, t);
 
     std::cout << data1[1].x << data1[1].y << std::endl;
 
